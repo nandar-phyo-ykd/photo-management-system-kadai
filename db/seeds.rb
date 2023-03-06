7419af053ee040f2763ca2a9503ea52a6bd5f8d6
Nandar <nandarphyo.ykd@gmail.com>
2023-03-07 08:14:21 +0900

    userモデル,migration作成やseedの修正 feature_user_account_management_issue_01

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(
+  [{ user_id: "test_1", password: "password012"},
+  { user_id: "test_2", password: "password123"},
+  { user_id: "test_3", password: "password456"},
+  { user_id: "test_4", password: "password789"}
+])
